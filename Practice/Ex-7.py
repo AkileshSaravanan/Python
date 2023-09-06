85d5d0c9703183b70a39a069574304a55549326e
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-09-06 12:49:42 +0530

    initial commit

---
+++ Practice/Ex-7.py
@@ -0,0 +1,27 @@
+'''
+Write a python program to print the sum of n numbers using looping constructs.
+
+Input format :
+Input consists of an integer 'n'.
+
+Output format :
+The output displays the sum of n numbers.
+
+Sample test cases :
+Input 1 :
+10
+
+Output 1 :
+55
+
+Input 2 :
+3
+
+Output 2 :
+6
+'''
+n=int(input())
+sum = 0
+for i in range(1,n+1):
+    sum = sum + i
+print(sum)
