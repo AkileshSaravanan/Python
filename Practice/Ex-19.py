de361c5da7180d1b1861a7ce6d00cf670fcf5fe0
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-09-26 10:59:43 +0530

    initial commit

---
+++ Practice/Ex-19.py
@@ -0,0 +1,25 @@
+'''
+Write a program that takes a number 
+�
+N as the input, and prints it to the output.
+
+Input Format
+The only line of input contains a single integer.
+
+Output Format
+Output the answer in a single line.
+
+Constraints
+0≤N≤10 
+5
+ 
+Sample 1:
+Input
+123
+
+Output
+123
+
+'''
+n=int(input())
+print(n)
