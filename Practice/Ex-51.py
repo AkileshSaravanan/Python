bb4eb269288b3198d8ba4126fd2eb584b2ea5213
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-01 15:10:11 +0530

    initial commit

---
+++ Practice/Ex-51.py
@@ -0,0 +1,35 @@
+'''
+CodeChef offers a feature called streak count. A streak is maintained if you solve at least one problem daily.
+Om and Addy actively maintain their streaks on CodeChef. Over a span of N consecutive days, you have observed the count of problems solved by each of them.
+Your task is to determine the maximum streak achieved by Om and Addy and find who had the longer maximum streak.
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of multiple lines of input.
+The first line of each test case contains an integer N — the number of days.
+
+Output Format
+For each test case, output:
+OM, if Om has longer maximum streak than Addy;
+ADDY, if Addy has longer maximum streak than Om;
+DRAW, if both have equal maximum streak.
+You may print each character in uppercase or lowercase. For example, OM, om, Om, and oM, are all considered the same.
+
+Sample 1:
+Input
+3
+6
+1 7 3 0 2 13
+0 2 3 4 5 0
+3
+1 3 4
+3 1 2
+5
+1 2 3 0 1
+1 2 0 2 3
+
+Output
+Addy
+Draw
+Om
+'''
