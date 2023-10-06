5de59f782e42f3e59f1101294cccadd9302a5e95
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-06 14:51:49 +0530

    initial commit

---
+++ Practice/Practice-1/Ex-61.py
@@ -0,0 +1,25 @@
+'''
+Note that there is a difference between Parameters and Arguments - actual values passed to a function.
+Check the code template below
+
+a, b are Parameters inside the function
+A, B are Arguments passed to the function
+def add_numbers(a, b):
+    return a + b
+
+# Calling the function with arguments
+A = 5
+B = 3
+result = add_numbers(A, B)
+print("Sum:", result)
+Task
+The code given in the IDE is incorrect.
+Can you debug the code to give the correct output?
+Check the input and expected output below.
+
+Sample 1:
+Input
+
+Output
+Hello, Alice!
+'''
