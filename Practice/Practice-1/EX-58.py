7a72d5e87aaf431809b8e57a076f7ce621165746
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-06 12:56:18 +0530

    initial commit

---
+++ Practice/Practice-1/EX-58.py
@@ -0,0 +1,19 @@
+'''
+Let us solve a simple problem to help us understand the importance of functions.
+
+Task
+Your are given 3 lines of input.
+Each line consists of 2 space separated integers - A and B
+Check the sample output below to understand this problem statement better.
+
+Sample 1:
+Input
+3 5
+2 7
+4 1
+
+Output
+2424194
+41902679
+5219
+'''
