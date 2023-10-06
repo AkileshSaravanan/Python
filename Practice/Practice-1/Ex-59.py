bbe1eb516a32dd9fe5b67c4b1269dfdaac419b17
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-06 12:58:39 +0530

    initial commit

---
+++ Practice/Practice-1/Ex-59.py
@@ -0,0 +1,19 @@
+'''
+Functions allow you to break down a complex program into smaller, manageable modules.
+Each function can represent a specific task or functionality.
+Once defined, functions can be reused in different parts of the program or even in different programs, promoting code reuse and saving development time.
+
+Look at the code in the IDE for the previous problem which highlights the implementation of functions.
+Review the code and click on Submit to proceed.
+
+Sample 1:
+Input
+3 5
+2 7
+4 1
+
+Output
+2424194
+41902679
+5219
+'''
