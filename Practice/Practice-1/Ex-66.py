9da1a144abd781fae4c34252e42dc90a56d90908
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-07 13:32:19 +0530

    initial commit

---
+++ Practice/Practice-1/Ex-66.py
@@ -0,0 +1,28 @@
+'''
+In the previous problem, we had 
+�
+t test cases and each test case had 1 line of input.
+However, each test case can have multiple lines of input as well.
+
+Task
+Lets write a program in the IDE which performs the following
+
+The 1st line of input is an integer t - the count of test cases
+Each test case consists of 2 lines of input
+The 1st line of input has 2 space separated integers - accept them as variables A and B
+The 2nd line of input has 3 space separated integers - accept them as variables C, D and E
+For each test case - output all integers on a single line
+Sample 1:
+Input
+3
+1 2
+3 4 5
+11 22
+33 44 55
+1 23
+456 789 101112
+Output
+1 2 3 4 5
+11 22 33 44 55
+1 23 456 789 101112
+'''
