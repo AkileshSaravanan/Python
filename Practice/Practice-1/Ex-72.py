d46c0ed90d8ca9e4bafc3f041098c572987d02b2
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-07 13:42:31 +0530

    initial commit

---
+++ Practice/Practice-1/Ex-72.py
@@ -0,0 +1,23 @@
+'''
+So as you solve programming problems - you will need to debug and find errors in your own code.
+
+Task
+You are given a program which does the following
+
+Accepts the count of test cases - t - in the 1st line
+The only line of each test case consists of an integer N
+For each test case, output to the console the value that is double the integer N
+Can you try and debug / fix the error in the given program?
+
+Sample 1:
+Input
+3
+1
+2
+3
+
+Output
+2
+4
+6
+'''
