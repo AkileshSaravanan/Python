5153fcb6daed45883e007d2f9e4eb1976697008a
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-07 13:33:39 +0530

    initial commit

---
+++ Practice/Practice-1/Ex-67.py
@@ -0,0 +1,25 @@
+'''
+In the previous problem, each testcase had 2 lines of input - each consisting of integers.
+Test cases can also contain a combination of integers and strings.
+
+Task
+Lets write a program in the IDE which performs the following
+
+The 1st line of input contains t - the count of testcases
+Each testcase consists of the following 2 lines of input
+The 1st line of the testcase contains 2 integers - accept them as variables A and B
+The 2nd line of the testcase contains 1 string - accept it as a variable S
+For each test case, output on one line the 2 integers followed by the string
+
+Sample 1:
+Input
+2
+1 2
+abcde
+34 567
+A1B2C3
+
+Output
+1 2 abcde
+34 567 A1B2C3
+'''
