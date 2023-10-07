0f9784945505f3764691a6b1bdd96703873a4170
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-07 13:38:09 +0530

    initial commit

---
+++ Practice/Practice-1/EX-69.py
@@ -0,0 +1,31 @@
+'''
+What is the difference between the Run and Submit buttons?
+
+Run allows you to locally test your code on sample inputs and custom inputs
+Custom inputs are small inputs - mainly for you to test your code
+Submit sends your code to the online judge to compare your Actual output and the Expected output
+On submission - your code is checked against thousands of test cases which are included in the hidden or private test files
+Your code / solution is complete only when you click Submit and get an AC (accepted) verdict
+Note: Your code can run as expected for your custom inputs - however, it can fail on the private test files.
+This means your code is NOT solving the problem correctly and needs to be fixed.
+Task
+You are given a program which does the following
+
+Accepts the count of test cases - t - in the 1st line.
+First line of each test case consists of an integer N
+Outputs the integer which is greater than N by 1
+Click on Run to check the result on custom inputs
+Click on Submit to check the final verdict
+
+Sample 1:
+Input
+3
+5
+11
+15
+
+Output
+6
+12
+16
+'''
