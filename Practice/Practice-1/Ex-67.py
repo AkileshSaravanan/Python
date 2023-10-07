3e6de0d41ec6a03c9b51360f7ce6f04ea6514c7d
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-07 13:33:56 +0530

    initial commit

--- Practice/Practice-1/Ex-67.py
+++ Practice/Practice-1/Ex-67.py
@@ -23,3 +23,12 @@ Output
 1 2 abcde
 34 567 A1B2C3
 '''
+t = int(input())
+#run a loop to accept 't' testcases
+for i in range(t):
+    #accept 2 integers on the 1st line of each test case
+    A, B = map(int,input().split())     
+    #accept 1 string on the 2nd line of each test case
+    C = input()
+    #output the 2 integers and 1 string on a single line for each test case
+    print(A, B, C) 
