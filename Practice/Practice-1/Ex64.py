c771c431d23e2c68d7b19944a8624386b4ea4496
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-07 13:24:00 +0530

    initial commit

---
+++ Practice/Practice-1/Ex64.py
@@ -0,0 +1,34 @@
+'''
+In the previous module, we practiced simple methods of input and output used in programming problems.
+
+Let us take the next small step and learn about test cases.
+You will find the concept of 'test cases' on various programming platforms such as Leetcode, Codeforces, CodeChef, Hackerrank. What are test cases?
+
+Test cases are multiple Inputs - multiple instances of the same problem, all of which have to be solved by your code correctly.
+Example 1: Consider 5 test cases or 5 inputs
+11
+13
+2
+4
+9
+Task
+Lets solve a simple problem.
+Write a program in the IDE which does the following
+
+Accepts 5 inputs given on 5 separate lines. Each input is an integer N
+For each test cases, prints out the integer N to console on a separate line (our Input mirror problem)
+Sample 1:
+Input
+11
+123
+34
+22
+45
+
+Output
+11
+123
+34
+22
+45
+'''
